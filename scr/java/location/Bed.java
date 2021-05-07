2abcd705bf5809f98d62cd9951b15973186e33a7
Fawziah Abdullah Alqarni (3761072) <79879305+FawziahAqarni@users.noreply.github.com>
2021-05-07 03:10:53 +0300

    Add files via upload

---
+++ scr/java/location/Bed.java
@@ -0,0 +1,57 @@
+package location;
+
+import java.io.Serializable;
+
+import patient.Patient;
+
+public class Bed implements Serializable{
+	private static final long serialVersionUID = 1L;
+	boolean vacant;
+	Patient patientAssigned;
+	int bedID;
+	/**
+	 * 
+	 */
+	public Bed(int bID) {
+		vacant = true;
+		bedID =bID;
+	}
+	/**
+	 * @return the vacant
+	 */
+	public boolean isVacant() {
+		return vacant;
+	}
+	/**
+	 * @param vacant the vacant to set
+	 */
+	public void setVacant(boolean vacant) {
+		this.vacant = vacant;
+	}
+	/**
+	 * @return the patientAssigned
+	 */
+	public Patient getPatientAssigned() {
+		return patientAssigned;
+	}
+	/**
+	 * @param patientAssigned the patientAssigned to set
+	 */
+	public void setPatientAssigned(Patient patientAssigned) {
+		this.patientAssigned = patientAssigned;
+	}
+	/**
+	 * @return the bedID
+	 */
+	public int getBedID() {
+		return bedID;
+	}
+	/**
+	 * @param bedID the bedID to set
+	 */
+	public void setBedID(int bedID) {
+		this.bedID = bedID;
+	}
+	
+	
+}
