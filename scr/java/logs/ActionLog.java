bdf3452206e0bf3779d82a25d7a8a90ac49e03f8
Fawziah Abdullah Alqarni (3761072) <79879305+FawziahAqarni@users.noreply.github.com>
2021-05-07 03:17:22 +0300

    Add files via upload

--- scr/java/logs/ActionLog.java
+++ scr/java/logs/ActionLog.java
@@ -1,1 +1,66 @@
+package logs;
 
+import java.io.Serializable;
+
+public class ActionLog implements Serializable {
+	private static final long serialVersionUID = 1L;
+	int staffID;
+	String day, time, actionPerformed;
+	
+	
+	/**
+	 * 
+	 */
+	public ActionLog() {
+	}
+	/**
+	 * @return the staffID
+	 */
+	public int getStaffID() {
+		return staffID;
+	}
+	/**
+	 * @param staffID the staffID to set
+	 */
+	public void setStaffID(int staffID) {
+		this.staffID = staffID;
+	}
+	/**
+	 * @return the day
+	 */
+	public String getDay() {
+		return day;
+	}
+	/**
+	 * @param day the day to set
+	 */
+	public void setDay(String day) {
+		this.day = day;
+	}
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return time;
+	}
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+	/**
+	 * @return the actionPerformed
+	 */
+	public String getActionPerformed() {
+		return actionPerformed;
+	}
+	/**
+	 * @param actionPerformed the actionPerformed to set
+	 */
+	public void setActionPerformed(String actionPerformed) {
+		this.actionPerformed = actionPerformed;
+	}
+	
+	
+}
