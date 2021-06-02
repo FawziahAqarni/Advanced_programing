4c54f7626d7e6de7f82a4798327037f4ea6dccc9
Fawziah Abdullah Alqarni (3761072) <79879305+FawziahAqarni@users.noreply.github.com>
2021-06-02 03:36:32 +0300

    Update Patient.java

--- scr/java/patient/Patient.java
+++ scr/java/patient/Patient.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Patient implements Serializable{
 	private static final long serialVersionUID = 1L;
 	int patientID;
